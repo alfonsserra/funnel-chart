544a0f11469ee5002220e2169ca868c7151566d3
alfonsserra <alfonsoserra@gmail.com>
2019-02-16 22:10:56 +0100

    Add systelab-componenents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,11 +15,15 @@ export class AppComponent implements OnInit {
 	}
 
 	public ngOnInit() {
-		this.data.push(new Level('Asset Discovery', 200000, '#15aed1'));
-		this.data.push(new Level('Asset Discovery', 400000, '#0c65a7'));
-		this.data.push(new Level('Asset Discovery', 500000, '#fda44a'));
-		this.data.push(new Level('Asset Discovery', 500000, '#88df75'));
-		this.data.push(new Level('Asset Discovery', 500000, '#149564'));
-		this.data.push(new Level('Asset Discovery', 10000, 'red'));
+		this.data.push(new Level('Sin resultado', 200000, '#15aed1'));
+		this.data.push(new Level('No validados', 400000, '#0c65a7'));
+		this.data.push(new Level('Invalidadas', 500000, 'red'));
+		this.data.push(new Level('Validados', 500000, '#88df75'));
+		this.data.push(new Level('Retenidos', 500000, '#fda44a'));
+		this.data.push(new Level('Impresos', 100000, '#149564'));
+	}
+
+	public doRefresh() {
+
 	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FunnelComponent } from './funnel/funnel.component';
+import { SystelabComponentsModule } from 'systelab-components';
+import { SystelabTranslateModule } from 'systelab-translate';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
 	declarations:    [
 		AppComponent,
-		FunnelComponent
+		FunnelComponent,
+		FilterComponent,
 	],
 	imports:         [
 		BrowserModule,
 		BrowserAnimationsModule,
+		SystelabTranslateModule.forRoot(),
+		SystelabComponentsModule.forRoot()
 	],
 	providers:       [],
 	bootstrap:       [AppComponent],
